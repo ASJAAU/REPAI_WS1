cf24e800addd66ed4fff515a478606d3a14601c0
Anders <asjo@create.aau.dk>
2024-04-05 15:45:01 +0200

    Added script to toggle expose feature of models

---
+++ toggle_featuremap_in_model_graph.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import yaml
+from models.resnet import *
+import argparse
+
+parser = argparse.ArgumentParser("Toggle the exposure of featuremap during forward pass of provided model")
+#Positionals
+parser.add_argument("config", type=str, help="Path to config file (YAML)")
+parser.add_argument("weights", type=str, help="Path to the model weight file")
+parser.add_argument("expose", type=bool, help="Whether to expose featuremap during forward pass")
+#Optionals
+parser.add_argument("output_name", type=str, default=None, help="Output name of new model graph [blank defaults to overiding]")
+
+args = parser.parse_args()
+with open ('config_personcls.yaml', 'r') as f:
+    cfg = yaml.safe_load(f)
+
+network = build_resnet_model(
+    input_shape=tuple(cfg["data"]["im_size"]),
+    depth = cfg["model"]["size"],
+    num_classes=len(cfg["model"]["classes"]),
+    expose_features=args.expose,
+    name=cfg["model"]["name"]
+)
+network.load_weights(args.weights)
+
+if args.output_name is not None:
+    tf.keras.models.save_model(network,args.output_name)
+else:
+    tf.keras.models.save_model(network,args.weights)
