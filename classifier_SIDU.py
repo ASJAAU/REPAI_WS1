ca3584dc510b94e1d91981a0572687cc4eda2912
Anders <asjo@create.aau.dk>
2024-04-12 13:50:31 +0200

    Inference + SIDU CLI sync

--- classifier_SIDU.py
+++ classifier_SIDU.py
@@ -5,14 +5,18 @@ import glob
 import os
 from utils.visualize import *
 from utils.metrics import Binary_Accuracy, binary_accuracy
+from utils.visualize import visualize_prediction
 import numpy as np
+import pandas as pd
 
-__EXT__ = [".jpg", ".png", ".bmp"]
+__EXT__ = (".jpg", ".png", ".bmp")
 
-parser = argparse.ArgumentParser("Run inference with a given model on select input data")
+parser = argparse.ArgumentParser("Run XAI with a given model on select input data")
 parser.add_argument("weights", type=str, help="Path to the model weight file")
 parser.add_argument("input", type=str, nargs= '+', help="Path to files/folders to run inference on")
 parser.add_argument("--save", type=str, default=None, help="Path to save the output of inference")
+parser.add_argument("--dataset_root", type=str, default="/Data/Harborfront_raw/", help="Path to dataset root, in case of loading datasplit")
+parser.add_argument("--classes", nargs='+', type=str, default=["human","bicycle", "motorcycle","vehicle"], help="List of class labels the model has been trained on")
 args = parser.parse_args()
 
 #Load modelweights
@@ -28,20 +32,40 @@ model.summary()
 
 print("##### LISTING DATA #####")
 images = []
+gts = []
+
 #list all valid inputs
 for input in args.input:
     if os.path.isfile(input) and input.endswith(__EXT__):
         images.append(input)
+        gts.append(None)
+    elif os.path.isfile(input) and input.endswith('.csv'):
+        split = pd.read_csv(input, sep=";")
+        split["label"] = split.apply(lambda x: [1 if int(x[g]) > 0 else 0 for g in args.classes], axis=1)
+        split["file_name"] = split.apply(lambda x: os.path.join(args.dataset_root, x["file_name"]), axis=1)
+        images.extend(split["file_name"].to_list())
+        gts.extend(split["label"].to_list())
     elif os.path.isdir(input):
         for ext in __EXT__:
-            images.extend(glob.glob(input + "/*" + ext))
+            valid_files_in_dir=glob.glob(input + "/*" + ext)
+            images.extend(valid_files_in_dir)
+            gts.extend([None] * len(valid_files_in_dir))
     else:
         print(f"Invalid input: '{input}'. Skipping")
 
+samples = zip(images, gts)
+print(len(images), len(gts))
+
 print("##### Explaining images #####")
-for img in images:
-    #load the image
-    img, x = load_img(read_path, (288,384))
+for img, label in samples:
+    #Load image
+    im = tf.keras.utils.load_img(img, color_mode='rgb', target_size=(288,384))
+    
+    #Normalize (0-1)
+    vis = tf.keras.utils.img_to_array(im)/255
+
+    #make into tensor (with batch dimension)
+    in_tensor = tf.convert_to_tensor(np.asarray([vis]))
 
     #extract predictions and featuremap
     pred_vec, feature_activation_maps = model.predict(x)
@@ -69,6 +93,7 @@ for img in images:
     sal, weights, new_interactions, diff_interactions, pred_org = explain_SIDU(model, x, N, 0.5, data, (288,384))
         
     #show explination
+    figure = visualize_prediction(img, pred_vec, label, sal[0], args.classes)
     plt.title(f'Does the image contain `cls_{class_idx}`')
     plt.axis('off')
     plt.imshow(img)
