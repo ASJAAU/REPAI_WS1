a1e9d9b29da6e50d88945b665583aed891964d6e
Anders <asjo@create.aau.dk>
2024-04-12 13:33:07 +0200

    Now accepts groundtruth predictions for visualization

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,17 +1,23 @@
 from matplotlib.pyplot import imshow, table, subplots, show
 
-def visualize_prediction(image, predictions, groundtruth=None, classes=["human", "bicycle", "motorcycle", "vehicle"]):
+def visualize_prediction(image, predictions, groundtruth=None, heatmap=None, classes=["human", "bicycle", "motorcycle", "vehicle"]):
     #Make figure
     fig, ax = subplots(1,2)
 
     #zip predictions with class names
     table_data = [[predictions[i],classes[i]] for i in range(len(predictions))]
 
+    #Format string for labels
+    text = [f"{classes[i]} - pred:{round(predictions[i],2)} gt: {groundtruth[i] if groundtruth is not None else ''}\n" for i in range(len(predictions))]
+    
     #Set image
     ax[0].imshow(image)
-    ax[1].table(table_data, loc='center')
+
+    #Set text
+    #ax[1].table(table_data, loc='center')
     ax[1].axis('off')
-    ax[1].axis('tight')
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax[1].text(0.05, 0.95, "".join(text), transform=ax[1].transAxes, fontsize=10, verticalalignment='center', bbox=props)
 
     #Show figure
     show()
