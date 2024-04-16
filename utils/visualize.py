172d3bf5fe8492bc4a655900a3671ee570ced691
Anders <asjo@create.aau.dk>
2024-04-16 16:04:15 +0200

    Now support variable length SIDU explinations (aswell as none)

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,26 +1,60 @@
-from matplotlib.pyplot import imshow, table, subplots, show
+from matplotlib.pyplot import subplots, show, figure
 
-def visualize_prediction(image, predictions, groundtruth=None, heatmap=None, classes=["human", "bicycle", "motorcycle", "vehicle"]):
-    #Make figure
-    fig, ax = subplots(1,2)
-
-    #zip predictions with class names
-    table_data = [[predictions[i],classes[i]] for i in range(len(predictions))]
-
-    #Format string for labels
-    text = [f"{classes[i]} - pred:{round(predictions[i],2)} gt: {groundtruth[i] if groundtruth is not None else ''}\n" for i in range(len(predictions))]
+def visualize_prediction(image, predictions, groundtruth=None, heatmaps=None, classes=["human", "bicycle", "motorcycle", "vehicle"]):
+    #Heatmaps?
+    len_heatmaps = len(heatmaps) if heatmaps is not None else 0
+    assert len_heatmaps <= len(classes), "Number of heatmaps may not exceed number of class names"
     
-    #Set image
-    ax[0].imshow(image)
+    #Make figure
+    fig, axs = subplots(1+len_heatmaps, 1, figsize=(6,6 * (len_heatmaps+1)))
 
-    #Set text
-    #ax[1].table(table_data, loc='center')
-    ax[1].axis('off')
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    ax[1].text(0.05, 0.95, "".join(text), transform=ax[1].transAxes, fontsize=10, verticalalignment='center', bbox=props)
+    #Remove graph ticks
+    if len_heatmaps < 1:
+        #Remove image ticks
+        axs.axis('off')
+
+        #Original image
+        axs.imshow(image)
+        axs.set_title("Input Image")
+
+        #Prediction
+        text = [f"{classes[i]} - pred:{round(float(predictions[i]),2)} gt: {groundtruth[i] if groundtruth is not None else ''}\n" for i in range(len(predictions))]
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
+        axs.text(0.05, 0.95, "".join(text), transform=axs.transAxes, fontsize=12, verticalalignment='top', bbox=props)
+    else:
+        #Remove image ticks
+        axs[0].axis('off')
+
+        #Original image
+        axs[0].imshow(image)
+        axs[0].set_title("Input Image")
+
+        #Prediction
+        text = [f"{classes[i]} - pred:{round(float(predictions[i]),2)} gt: {groundtruth[i] if groundtruth is not None else ''}\n" for i in range(len(predictions))]
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
+        axs[0].text(0.01, 0.99, "".join(text), transform=axs[0].transAxes, fontsize=12, verticalalignment='top', bbox=props)
+   
+
+    #Visualize heatmaps
+    print(f"LENGTH : {len_heatmaps}")
+    if len_heatmaps >= 1:
+        for i in range(len_heatmaps):
+            #Remove graph ticks
+            axs[i+1].axis('off')
+            #Set image
+            axs[i+1].set_title(f"Heatmap: {classes[i]}")
+            axs[i+1].imshow(image, aspect='equal')
+            #Set heatmap
+            hmap = axs[i+1].imshow(heatmaps[i], cmap='jet', alpha=0.5)
+            #Plot Colorbar / colormap
+            cax = axs[i+1].inset_axes([0.2, -0.04, 0.6, 0.04], transform=axs[i+1].transAxes)
+            fig.colorbar(hmap, cax=cax, orientation='horizontal')
 
     #Show figure
     show()
+
+    #Compress padding
+    fig.tight_layout()
     
     #Return figure for possible saving
     return fig
