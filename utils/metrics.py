540e8c3e8e2f718dd6215cbdef44138d9afd80ec
Anders <asjo@create.aau.dk>
2024-05-27 10:39:56 +0200

    Added RMSE and MAE

--- utils/metrics.py
+++ utils/metrics.py
@@ -18,12 +18,45 @@ def binary_accuracy(y_true, y_pred, threshold=0.5, element=None):
     else:
         return metrics_utils.binary_matches(tf.transpose(y_true[:,element]), tf.transpose(y_pred[:,element]), threshold=threshold)
 
+class Mean_Absolute_Error(MeanMetricWrapper):
+    def __init__(self, name="mean absolute error", dtype=None, element=None):
+        super().__init__(fn=partial(mae, element=element), name=name, dtype=dtype)
+        # Metric should be maximized during optimization.
+        self._direction = "up"
+
+    def get_config(self):
+        return {"name": self.name, "dtype": self.dtype}
+
+def mae(y_true, y_pred, element=None):
+    if element == None:
+        return tf.reduce_mean(abs(y_true - y_pred))
+    else:
+        return abs(tf.transpose(y_true[:,element]) - tf.transpose(y_pred[:,element]))
+
+class Root_Mean_Squared_Error(MeanMetricWrapper):
+    def __init__(self, name="mean squared error", dtype=None, element=None):
+        super().__init__(fn=partial(mae, element=element), name=name, dtype=dtype)
+        # Metric should be maximized during optimization.
+        self._direction = "up"
+
+    def get_config(self):
+        return {"name": self.name, "dtype": self.dtype}
+
+def rmse(y_true, y_pred, element=None):
+    if element == None:
+        return tf.math.sqrt(tf.reduce_mean(abs(y_true - y_pred)**2))
+    else:
+        return tf.math.sqrt(tf.reduce_mean(abs(tf.transpose(y_true[:,element]) - tf.transpose(y_pred[:,element]))**2))
+
+
+
+
 if __name__ == '__main__':
     pred = [
         [0,1,0,0],
         [1,1,0,0],
         [1,1,0,0],
-        [1,1,0,0],
+        [1,1,2,0],
         ]
     gt   = [
         [1,1,0,0],
@@ -32,7 +65,7 @@ if __name__ == '__main__':
         [1,1,0,0],
         ]
     print("Verifying functionality of metrics")
-    m = Binary_Accuracy(element=2)
+    m = Root_Mean_Squared_Error(element=2)
     m.update_state(pred, gt)
     m.result()
     print(m.result())
